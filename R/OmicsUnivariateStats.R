42cde504ff9d8b9f0edf96ae12d0cb6d1619fd67
Federico Martinez-Seidel <54093698+MSeidelFed@users.noreply.github.com>
2021-07-09 10:54:22 +0200

    add U norm & 0 replacement

--- R/OmicsUnivariateStats.R
+++ R/OmicsUnivariateStats.R
@@ -16,7 +16,8 @@ OmicsUnivariateStats <- function(class_comparison_mat = abs(distribution_test_ma
                                  Factor1,
                                  Factor2 = NULL,
                                  Contrast = F,
-                                 ReturnTukeyPlots = T) {
+                                 ReturnTukeyPlots = T,
+                                 marginsTukey = c(6,12,3,3)) {
   
   ## functions needed
   
@@ -28,40 +29,61 @@ OmicsUnivariateStats <- function(class_comparison_mat = abs(distribution_test_ma
   }
   
   ## main
-
-
+  
+  
   if (dim(class_comparison_mat)[1] == dim(distribution_test_mat())[1] &
       dim(class_comparison_mat)[2] == dim(distribution_test_mat())[2]) {
-
+    
     class_comparison_mat[which(class_comparison_mat == 0)] <- 0.0001
-
+    
   }
-
+  
   ## defining the formula for the models
-
+  
   if (length(Factor2) > 1) {
     
     FactorNo <- 2
-
-    if (Contrast == T) { Formula = class_comparison_mat[,i] ~ Factor1 * Factor2}
-    else {  Formula = class_comparison_mat[,i] ~ Factor1 + Factor2 }
-
-    Levene_factor = paste(Factor1, Factor2, sep = "_")
-
+    
+    if (Contrast == T) {Formula = class_comparison_mat[,i] ~ Factor1 * Factor2} else {  
+      
+      Formula = class_comparison_mat[,i] ~ Factor1 + Factor2 
+      
+      }
+    
+      Levene_factor = as.factor(paste(Factor1, Factor2, sep = "_"))
+    
   } else {
     
     FactorNo <- 1
-
+    
     Formula = class_comparison_mat[,i] ~ Factor1
     Levene_factor = Factor1
-
+    
   }
   
   print(Formula)
   
   ## removing all kind of error prone features from the input matrices
   
-  #print(dim(class_comparison_mat))
+  #### unity-based normalization
+  
+  mat <- class_comparison_mat
+  
+  mat_Ubased_norm <- (mat - rep(colMins(mat, na.rm = T),
+                                rep.int(nrow(mat),
+                                        ncol(mat)))) / (rep(colMaxs(mat, na.rm = T),
+                                                            rep.int(nrow(mat),
+                                                                    ncol(mat))) - rep(colMins(mat, na.rm = T),
+                                                                                      rep.int(nrow(mat),
+                                                                                              ncol(mat))))
+  
+  #### zero replacement by small values
+  
+  mat_Ubased_norm[which(mat_Ubased_norm == 0)] <- 0.0000000000001
+  
+  
+  class_comparison_mat <- mat_Ubased_norm
+  
   
   ## remove rows full of NAs
   
@@ -73,41 +95,48 @@ OmicsUnivariateStats <- function(class_comparison_mat = abs(distribution_test_ma
     cat("...", "\n")
     
     class_comparison_mat = as.matrix(class_comparison_mat[,-c(indexes_NAs)])
-    
-    #print(dim(class_comparison_mat))
   }
   
   ## Levene´s test produces errors when there is no variance between treatments, 
   ## here we remove features with no sd in individual treatments
+  ## aggregate by rep number to ensure enough reps for SD and full coverage among treatments
   
   mean_treatment_sd <- c()
-  
+  min_treatment_sd <- c()
   NA_remover <- c()
   
   for (i in 1:dim(class_comparison_mat)[2]) {
     
     mean_treatment_sd[i] <- mean(aggregate(Formula, FUN = sd)[,(FactorNo + 1)])
     
-    ## test 1 gets factors with no replication out of the question
+    treatments <- aggregate(Formula, FUN = length)[, 
+                                                   (FactorNo + 1)]
     
-    test1NA <- any(is.na(aggregate(Formula, FUN = mean)[,(FactorNo + 1)]))
+    treatment_length <- length(treatments)
     
-    ## test 2 is necessary because when only few factors have the reps the no NAs will appear in the final object
-      
-    test2NA <- dim(aggregate(Formula, FUN = mean))[1] != length(levels(as.factor(paste0(Factor1 ,Factor2))))
+    min_treatment_sd[i] <- min(treatments)
     
-    if (test1NA | test2NA) {
-      
+    if (min_treatment_sd[i] < 3 | treatment_length < 4) {
       NA_remover <- c(NA_remover, i)
-      
     }
     
-    #print(mean_treatment_sd[i])
   }
   
-  null_sd_features <- colnames(class_comparison_mat[,which(mean_treatment_sd == 0)])
+  ### removing features with not enough replication due to NAs
   
-  #print(null_sd_features)
+  NA_features <- colnames(class_comparison_mat[,NA_remover])
+  
+  if (length(NA_features) > 0) {
+    
+    cat("Features without replication due to NAs : ", "\n", NA_features)
+    cat("...", "\n")
+    
+    class_comparison_mat = as.matrix(class_comparison_mat[,-c(NA_remover)])
+  }
+  
+  #### null variance in treatments
+  
+  null_sd_features <- colnames(class_comparison_mat[,which(mean_treatment_sd == 0)])
   
   if (length(null_sd_features) > 0) {
     
@@ -116,25 +145,7 @@ OmicsUnivariateStats <- function(class_comparison_mat = abs(distribution_test_ma
     
     class_comparison_mat = as.matrix(class_comparison_mat[,-c(which(mean_treatment_sd == 0))])
     
-    #print(dim(class_comparison_mat))
   }
-  
-  ### removing features with not enough replication due to NAs
-  
-  NA_features <- colnames(class_comparison_mat[,NA_remover])
-  
-  #print(NA_features)
-  
-  if (length(NA_features) > 0) {
-    
-    cat("Features without replication due to NAs : ", "\n", NA_features)
-    cat("...", "\n")
-    
-    class_comparison_mat = as.matrix(class_comparison_mat[,-c(NA_remover)])
-    
-    #print(dim(class_comparison_mat))
-  }
-  
   
   ### removing features with global null standard deviation
   
@@ -148,14 +159,16 @@ OmicsUnivariateStats <- function(class_comparison_mat = abs(distribution_test_ma
     
   }
   
+  ## defining regression families
+  
   regfamily <- testing_distributions(Distribution_test_mat = class_comparison_mat)
   
   ## for loop for tests
-
+  
   ### empty mat to store results
-
+  
   i = 1
-
+  
   test_out <- matrix(NA,
                      nrow = dim(class_comparison_mat)[2],
                      ncol = length(c(glm(Formula)[["coefficients"]],
@@ -164,21 +177,21 @@ OmicsUnivariateStats <- function(class_comparison_mat = abs(distribution_test_ma
   TukeyHSD_info <- matrix(NA,
                           nrow = dim(class_comparison_mat)[2],
                           ncol = length(levels(Levene_factor)))
-
+  
   count = 0
   
   if(ReturnTukeyPlots == T) {
     
     pdf("TukeyHSD_Plots.pdf")
     
-    par(mar=c(7,10,2,2))
+    par(mar = marginsTukey)
     
     par(mfrow = c(2,1))
     
   }
   
   for (i in 1:dim(class_comparison_mat)[2]) {
-
+    
     count = count + 1
     
     ## TukeyPlots
@@ -194,64 +207,64 @@ OmicsUnivariateStats <- function(class_comparison_mat = abs(distribution_test_ma
     }
     
     print(c(regfamily[i], "Column Number", count))
-
+    
     if (levene.test(y = class_comparison_mat[,i],
                     group = Levene_factor,
                     location = "median",
                     bootstrap = F)[["p.value"]] > 0.05 &
         shapiro.test(x = class_comparison_mat[,i])[["p.value"]] > 0.05) {
-
-
-        test <- c(summary(lm(Formula))$coef[1:dim(summary(glm(Formula,
-                                                              gaussian))$coef)[1],
-                                            "Pr(>|t|)"],
-                  "Homoscedastic & Parametric")
-
-
+      
+      
+      test <- c(summary(lm(Formula))$coef[1:dim(summary(glm(Formula,
+                                                            gaussian))$coef)[1],
+                                          "Pr(>|t|)"],
+                "Homoscedastic & Parametric")
+      
+      
     } else if (levene.test(y = class_comparison_mat[,i],
-                             group = Levene_factor,
-                             location = "median",
-                             bootstrap = F)[["p.value"]] < 0.05 &
-                 shapiro.test(x = class_comparison_mat[,i])[["p.value"]] > 0.05) {
-
-          test <- c(summary(lm(Formula))$coef[1:dim(summary(glm(Formula,
-                                                    gaussian))$coef)[1],
-                                              "Pr(>|t|)"],
-                    "Heteroscedastic & Parametric")
-
-
+                           group = Levene_factor,
+                           location = "median",
+                           bootstrap = F)[["p.value"]] < 0.05 &
+               shapiro.test(x = class_comparison_mat[,i])[["p.value"]] > 0.05) {
+      
+      test <- c(summary(lm(Formula))$coef[1:dim(summary(glm(Formula,
+                                                            gaussian))$coef)[1],
+                                          "Pr(>|t|)"],
+                "Heteroscedastic & Parametric")
+      
+      
     } else if (levene.test(y = class_comparison_mat[,i],
-                              group = Levene_factor,
-                              location = "median",
-                              bootstrap = F)[["p.value"]] > 0.05 &
-                  shapiro.test(x = class_comparison_mat[,i])[["p.value"]] < 0.05) {
+                           group = Levene_factor,
+                           location = "median",
+                           bootstrap = F)[["p.value"]] > 0.05 &
+               shapiro.test(x = class_comparison_mat[,i])[["p.value"]] < 0.05) {
       
       test <- c(summary(glm(Formula, regfamily[i]))$coef[1:dim(summary(glm(Formula,
-                                                                             regfamily[i]))$coef)[1],
-                                                           "Pr(>|t|)"],
-                  "Homoscedastic & Non-parametric")
-
+                                                                           regfamily[i]))$coef)[1],
+                                                         "Pr(>|t|)"],
+                "Homoscedastic & Non-parametric")
+      
     } else if (levene.test(y = class_comparison_mat[,i],
-                              group = Levene_factor,
-                              location = "median",
-                              bootstrap = F)[["p.value"]] < 0.05 &
-                  shapiro.test(x = class_comparison_mat[,i])[["p.value"]] < 0.05) {
-
-          test <- c(summary(glm(Formula, regfamily[i]))$coef[1:dim(summary(glm(Formula,
-                                                                        regfamily[i]))$coef)[1],
-                                                      "Pr(>|t|)"],
-                    "Heteroscedastic & Non-parametric")
-
+                           group = Levene_factor,
+                           location = "median",
+                           bootstrap = F)[["p.value"]] < 0.05 &
+               shapiro.test(x = class_comparison_mat[,i])[["p.value"]] < 0.05) {
+      
+      test <- c(summary(glm(Formula, regfamily[i]))$coef[1:dim(summary(glm(Formula,
+                                                                           regfamily[i]))$coef)[1],
+                                                         "Pr(>|t|)"],
+                "Heteroscedastic & Non-parametric")
+      
     }
-
+    
     test_out[i,] <- test
-
+    
     colnames(test_out) <- c(names(glm(Formula,
                                       gaussian)[["coefficients"]]),
                             "Assumptions_tested")
-
+    
   }
-
+  
   rownames(test_out) <- colnames(class_comparison_mat)
   
   if(ReturnTukeyPlots == T) {
@@ -259,19 +272,19 @@ OmicsUnivariateStats <- function(class_comparison_mat = abs(distribution_test_ma
     dev.off()
     
   }
-
+  
   ### FDR adjustment
-
+  
   FDR_adjustment <- matrix(NA, nrow = dim(test_out)[1], ncol = (dim(test_out)[2]-1))
-
+  
   for (j in 1:(dim(test_out)[2]-1)) {
-
+    
     runner <- p.adjust(test_out[,j], method = "fdr")
-
+    
     FDR_adjustment[,j] <- runner
-
+    
   }
-
+  
   
   if(ReturnTukeyPlots == T) {
     
@@ -308,9 +321,9 @@ OmicsUnivariateStats <- function(class_comparison_mat = abs(distribution_test_ma
                                           sep = "_"))
     
   }
-
   
-
+  
+  
   return(adjusted_test_out)
-
+  
 }
